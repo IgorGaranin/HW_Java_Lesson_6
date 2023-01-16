4587cf81f1cd3cbc2eeffb4b41e2d6c78d873e6c
IgorGaranin <igorgaranin490@gmail.com>
2023-01-16 07:58:36 +0300

    initial commit

--- HW_Java_Lesson_6/src/main/java/study/Programm.java
+++ HW_Java_Lesson_6/src/main/java/study/Programm.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Programm {
+package study;
+
+public class Programm {
+    public static void main(String[] args) {
+        System.out.println("worked!");
+    }
 }
