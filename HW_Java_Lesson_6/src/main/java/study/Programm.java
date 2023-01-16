d7fcfdd8ff6e758aa5251a539a4adda5fd5f77b1
IgorGaranin <igorgaranin490@gmail.com>
2023-01-16 10:38:16 +0300

    add Task3

--- HW_Java_Lesson_6/src/main/java/study/Programm.java
+++ HW_Java_Lesson_6/src/main/java/study/Programm.java
@@ -28,8 +28,20 @@ public class Programm {
         //output
         System.out.println("output");
         int maxValueT2 = searchMaxValueIntArr(arrForSearchMaxValue);
-        System.out.println("Максимальное значение: " + maxValueT2)
+        System.out.println("Максимальное значение: " + maxValueT2);
 
+        System.out.println("////////////////////////////////////////////////\n");
+        // Task 3
+        System.out.println("Task3");
+        // input
+        System.out.println("input");
+        int lenghtArrayT3 = 10;
+        int[] arrForSearchMinValue = createRandomIntArray(lenghtArrayT3);
+        System.out.println("Массив: " + Arrays.toString(arrForSearchMinValue));
+        // output
+        System.out.println("output");
+        int minValueT3 = searchMinValueIntArr(arrForSearchMinValue);
+        System.out.println("Минимальное значение: " + minValueT3);
 
 
     }
@@ -44,11 +56,23 @@ public class Programm {
     }
 
     public static int searchMaxValueIntArr(int[] intArrForSearchMaxValue) {
-        int result = 0;
+        int result = intArrForSearchMaxValue[0];
 
         for (int i = 0; i < intArrForSearchMaxValue.length; i++) {
             if (result < intArrForSearchMaxValue[i]) {
                 result = intArrForSearchMaxValue[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static int searchMinValueIntArr(int[] arrForSearchMinValue){
+        int result = arrForSearchMinValue[0];
+
+        for (int i = 0; i < arrForSearchMinValue.length; i++) {
+            if (result > arrForSearchMinValue[i]) {
+                result = arrForSearchMinValue[i];
             }
         }
 
