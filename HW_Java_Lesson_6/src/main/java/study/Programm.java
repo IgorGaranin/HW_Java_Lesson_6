fd39fdac319b3620f6a2fc66a052f3eec39cb53b
IgorGaranin <igorgaranin490@gmail.com>
2023-01-16 09:11:09 +0300

    Task1

--- HW_Java_Lesson_6/src/main/java/study/Programm.java
+++ HW_Java_Lesson_6/src/main/java/study/Programm.java
@@ -1,7 +1,33 @@
 package study;
 
+import java.util.Arrays;
+
 public class Programm {
     public static void main(String[] args) {
-        System.out.println("worked!");
+
+        //Task 1
+        System.out.println("Task 1");
+        //input
+        System.out.println("input");
+        int lenghtArrayT1 = 5;
+        System.out.println("Длина массива 5");
+        // output
+        System.out.println("output");
+        int[] randomIntArr = createRandomIntArray(lenghtArrayT1);
+        System.out.println(Arrays.toString(randomIntArr));
+
+        System.out.println("////////////////////////////////////////////////\n");
+
+
+
+    }
+
+    public static int[] createRandomIntArray(int lenghtArray){
+        int[] result = new int[lenghtArray];
+        for (int i = 0; i < result.length; i++) {
+            result[i] =(int)(Math.random()*100);
+        }
+
+        return result;
     }
 }
