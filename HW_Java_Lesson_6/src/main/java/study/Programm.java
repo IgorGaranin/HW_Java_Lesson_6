06095e133f55e34dd4b60391226d34659ded7400
IgorGaranin <igorgaranin490@gmail.com>
2023-01-16 09:43:27 +0300

    add Task2

--- HW_Java_Lesson_6/src/main/java/study/Programm.java
+++ HW_Java_Lesson_6/src/main/java/study/Programm.java
@@ -18,14 +18,38 @@ public class Programm {
 
         System.out.println("////////////////////////////////////////////////\n");
 
+        //Task 2
+        System.out.println("Task2");
+        //input
+        System.out.println("input");
+        int lenghtArrayT2 = 10;
+        int[] arrForSearchMaxValue = createRandomIntArray(lenghtArrayT2);
+        System.out.println("Массив: " + Arrays.toString(arrForSearchMaxValue));
+        //output
+        System.out.println("output");
+        int maxValueT2 = searchMaxValueIntArr(arrForSearchMaxValue);
+        System.out.println("Максимальное значение: " + maxValueT2)
+
 
 
     }
 
-    public static int[] createRandomIntArray(int lenghtArray){
+    public static int[] createRandomIntArray(int lenghtArray) {
         int[] result = new int[lenghtArray];
         for (int i = 0; i < result.length; i++) {
-            result[i] =(int)(Math.random()*100);
+            result[i] = (int) (Math.random() * 100);
+        }
+
+        return result;
+    }
+
+    public static int searchMaxValueIntArr(int[] intArrForSearchMaxValue) {
+        int result = 0;
+
+        for (int i = 0; i < intArrForSearchMaxValue.length; i++) {
+            if (result < intArrForSearchMaxValue[i]) {
+                result = intArrForSearchMaxValue[i];
+            }
         }
 
         return result;
